ff4c96f847d4136fcd36fe13a986db746c42c40f
wldh25 <wldh25@hufs.ac.kr>
2025-10-20 21:19:45 +0900

    feat : 기본 구분자(, :) 입력받았을때 합치기 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,26 @@
 package calculator;
 
+import calculator.parser.InputParser;
+import calculator.validator.InputValidator;
+
 public class StringAddCalculator {
     private StringAddCalculator() { }
     public static int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0; // 빈 문자열은 0 출력
         }
-        // 예외 처리
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        // 기본 구분자로 분리
+        String[] tokens = InputParser.splitByDefault(input);
+        
+        // 검증
+        InputValidator.validateTokens(tokens);
+        
+        // 합치기
+        long sum = 0L;
+        for(String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
+        return (int) sum;
     }
 }
