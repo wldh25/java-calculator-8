8968f7c9f1053189f816ff962ea3dc655e10966a
wldh25 <wldh25@hufs.ac.kr>
2025-10-20 22:22:51 +0900

    fix : IllegalArgumentException을 validator에서 처리하게끔 해주기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,12 +7,8 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        // 예외 발생 시 프로그램이 종료되도록 처리
-        try {
-            int result = StringAddCalculator.add(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            // 별도 메세지 출력 없이 바로 종료
-        }
+        int result = StringAddCalculator.add(input);
+        System.out.println("결과 : " + result);
+
     }
 }
