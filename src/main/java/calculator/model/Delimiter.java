edcfb5117509cb3de00fbd9ab63747abfe848e08
wldh25 <wldh25@hufs.ac.kr>
2025-10-20 21:11:06 +0900

    feat : 기본 구분자 모델 구현

---
+++ src/main/java/calculator/model/Delimiter.java
@@ -0,0 +1,9 @@
+package calculator.model;
+
+public class Delimiter {
+    private Delimiter() {}
+
+    public static final String DEFAULT_REGEX = ",|:";
+    public static final String CUSTOM_PREFIX = "//";
+    public static final char NEWLINE = '\n';
+}
