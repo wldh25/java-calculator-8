afbfca8bbe05957af07e233598d6c9c8b0db8c6f
wldh25 <wldh25@hufs.ac.kr>
2025-10-20 21:29:07 +0900

    refactor : StringAddCalculator 함수명 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,19 +4,18 @@ import calculator.parser.InputParser;
 import calculator.validator.InputValidator;
 
 public class StringAddCalculator {
+
     private StringAddCalculator() { }
+
     public static int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0; // 빈 문자열은 0 출력
         }
         
-        // 기본 구분자로 분리
-        String[] tokens = InputParser.splitByDefault(input);
-        
-        // 검증
+        // 구분자에 따른 분리 및 검증
+        String[] tokens = InputParser.split(input);
         InputValidator.validateTokens(tokens);
         
-        // 합치기
         long sum = 0L;
         for(String token : tokens) {
             sum += Integer.parseInt(token);
